4c0c3e3d8f52f9b0ad44e6c5e1ab1dc811a5f002
John Hardy <john.hardy@workjam.com>
2021-04-25 17:11:15 +1000

    reorg

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const readlineSync = require('readline-sync');
+
+clear();
+
+console.log(
+    chalk.yellow(
+        figlet.textSync('webforth-cli', { horizontalLayout: 'full' })
+    )
+);
+
+ 
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+ 
+// Handle the secret text (e.g. password).
+var favFood = readlineSync.question('What is your favorite food? ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+});
+console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+
+if (readlineSync.keyInYN('Do you want this module?')) {
+    // 'Y' key was pressed.
+    console.log('Installing now...');
+    // Do something...
+  } else {
+    // Another key was pressed.
+    console.log('Searching another...');
+    // Do something...
+  }
+  
